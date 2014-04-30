817c8f1ba3fc5d34ca61747cf7f7c5a4473e75bf
Chris Marasco <cxm6467@gmail.com>
2014-04-30 10:00:42 -0400

    Basics.

--- powerGet.ps1
+++ powerGet.ps1
@@ -1,3 +1,4 @@
+clear
 <#
 .SYNOPSIS
 	<>  
@@ -12,3 +13,36 @@
 .LINK 
 	<>
 #>
+
+## Variables ##
+$fileContents = @()
+
+try{
+
+    $fileForLinks = ".\Data\fileLocations.txt"
+    readFile($fileForLinks)
+
+
+}Catch [System.IO.FileNotFoundException]{
+    Write-Debug "File not found"
+    Write-EventLog $_.Exception.Message
+
+}Catch [Exception]{
+    Write-Debug "Unhandled error caught"
+    Write-EventLog Exception.Message
+}
+
+function readFile($file){
+    #make that a required param
+
+    $fileContents = Get-Content($file)
+
+    proccessLinks($fileContents)
+}
+
+function proccessLinks($list){
+
+    foreach($item in $list){
+        Write-Host("Processing $item")
+    }
+}
